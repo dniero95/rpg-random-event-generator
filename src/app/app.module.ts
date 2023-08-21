57025307ba96f1954e03fa3249c5cc9a907e6458
Dario Niero <dario.niero@enginius.com>
2023-08-21 15:03:06 +0200

    WIP: generate random table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RandomTableComponent } from './components/random-table/random-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RandomTableComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/random-table/random-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-random-table',
+  templateUrl: './random-table.component.html',
+  styleUrls: ['./random-table.component.scss']
+})
+export class RandomTableComponent {
+
+}
