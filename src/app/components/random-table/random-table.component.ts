53dcb16101fa689590d9f2ba7b9cd02ce1d07605
Dario Niero <dario.niero@enginius.com>
2023-08-21 18:26:11 +0200

    basic implementation fixed

--- src/app/components/random-table/random-table.component.ts
+++ src/app/components/random-table/random-table.component.ts
@@ -1,19 +1,53 @@
-import { Component, OnInit, inject } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  inject,
+} from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { VampireDowntimeElement } from 'src/app/interfaces/vampire-downtime-element';
 import { RowService } from 'src/app/services/row.service';
-
 
 @Component({
   selector: 'app-random-table',
   templateUrl: './random-table.component.html',
   styleUrls: ['./random-table.component.scss'],
 })
-export class RandomTableComponent implements OnInit {
-  displayedColumns: string[] = ['position','Turf', 'Event type', 'Reward', 'Reward Number Roll', 'NPC Faction', 'Required Roll'];
-  dataSource!:VampireDowntimeElement[];
+export class RandomTableComponent implements OnInit, OnChanges {
+  displayedColumns: string[] = [
+    'position',
+    'Turf',
+    'Event type',
+    'Reward',
+    'Reward Number Roll',
+    'NPC Faction',
+    'Required Roll',
+  ];
+  // dataSource!:VampireDowntimeElement[];
+  dataSource = new MatTableDataSource<VampireDowntimeElement>([]);
   public rowService = inject(RowService);
+  private cdr = inject(ChangeDetectorRef);
 
   ngOnInit(): void {
-    this.dataSource = this.rowService.vampireDowntimeRows;
+    // this.dataSource = this.rowService.vampireDowntimeRows;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    for (const propName in changes) {
+      console.log(propName);
+    }
+  }
+
+  test() {
+    // this.rowService.generateRandomRow();
+    // // this.dataSource = this.rowService.vampireDowntimeRows;
+    // this.cdr.detectChanges();
+    // console.log(this.dataSource);
+
+    this.rowService.doSomething().subscribe((data: VampireDowntimeElement[]) => {
+      this.dataSource.data = data;
+    });
   }
 }
--- src/app/services/row.service.ts
+++ src/app/services/row.service.ts
@@ -1,40 +1,79 @@
 import { Injectable } from '@angular/core';
-import { dsv } from 'd3-fetch'
+import { dsv } from 'd3-fetch';
 import { VampireDowntimeElement } from '../interfaces/vampire-downtime-element';
+import { Observable, of } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RowService {
+  constructor() {}
 
-  constructor() { }
+  vampireDowntimeRows: VampireDowntimeElement[] = [];
 
-  data = dsv(';','../../assets/tavola.csv');
+  async generateRandomRow() {
+    const data = await dsv(';', '../../assets/tavola.csv');
 
-  vampireDowntimeRows: VampireDowntimeElement[] = [
-    {
-      position: 0,
-      turf: '19th Floor',
-      eventType: 'Assault',
-      reward: 'Coin',
-      rewardNumberRoll: 1,
-      npcFaction: 'Werewolves',
-      requiredRoll: 'Physical',
-    },
-  ];
+    const turfes: string[] = [];
+    const eventTypes: string[] = [];
+    const rewards: string[] = [];
+    const rewardNumberRolls: number[] = [];
+    const npcFactions: string[] = [];
+    const requiredRolls: string[] = [];
 
-  generateRandomRow():VampireDowntimeElement{
-    const row: VampireDowntimeElement = {
-      position: 0,
-      turf: '19th Floor',
-      eventType: 'Assault',
-      reward: 'Coin',
-      rewardNumberRoll: 1,
-      npcFaction: 'Werewolves',
-      requiredRoll: 'Physical',
+    let row: VampireDowntimeElement;
+
+    data.forEach((row) => {
+      if (typeof row['Turf'] === 'string' && row['Turf'] !== '') {
+        turfes.push(row['Turf']);
+      }
+      if (typeof row['Event Type'] === 'string' && row['Event Type'] !== '') {
+        eventTypes.push(row['Event Type']);
+      }
+      if (typeof row['Reward'] === 'string' && row['Reward'] !== '') {
+        rewards.push(row['Reward']);
+      }
+      if (
+        typeof row['Reward Number Roll'] === 'string' &&
+        row['Reward Number Roll'] !== ''
+      ) {
+        rewardNumberRolls.push(+row['Reward Number Roll']);
+      }
+      if (typeof row['NPC Faction'] === 'string' && row['NPC Faction'] !== '') {
+        npcFactions.push(row['NPC Faction']);
+      }
+      if (
+        typeof row['Required Roll'] === 'string' &&
+        row['Required Roll'] !== ''
+      ) {
+        requiredRolls.push(row['Required Roll']);
+      }
+    });
+
+    row = {
+      position: this.vampireDowntimeRows.length,
+      turf: turfes[Math.floor(Math.random() * turfes.length)],
+      eventType: eventTypes[Math.floor(Math.random() * eventTypes.length)],
+      reward: rewards[Math.floor(Math.random() * rewards.length)],
+      rewardNumberRoll:
+        rewardNumberRolls[Math.floor(Math.random() * rewardNumberRolls.length)],
+      npcFaction: npcFactions[Math.floor(Math.random() * npcFactions.length)],
+      requiredRoll:
+        requiredRolls[Math.floor(Math.random() * requiredRolls.length)],
     };
-    console.log(row);
-    
-    return row;
+
+    this.vampireDowntimeRows.push(row);
+
+    // console.log(this.vampireDowntimeRows);
+    // console.log(this.data);
+
+    // return row;
   }
 
+  doSomething(): Observable<VampireDowntimeElement[]> {
+    // commonly something like:
+    // return this.httpClient.get('https://example.org/rest-api/items/');
+    this.generateRandomRow();
+
+    return of(this.vampireDowntimeRows);
+  }
 }
