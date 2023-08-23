682f76916275589d8747605ce0ca2a0847aa885b
Dario Niero <dario.niero@enginius.com>
2023-08-23 15:22:01 +0200

    WIP: swtich to AfterViewInit lifecycle

--- src/app/components/random-table/random-table.component.ts
+++ src/app/components/random-table/random-table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, inject } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { VampireDowntimeElement } from 'src/app/interfaces/vampire-downtime-element';
 import { RowService } from 'src/app/services/row.service';
@@ -8,13 +9,24 @@ import { RowService } from 'src/app/services/row.service';
   templateUrl: './random-table.component.html',
   styleUrls: ['./random-table.component.scss'],
 })
-export class RandomTableComponent implements OnInit{
-  ngOnInit(): void {
+export class RandomTableComponent implements AfterViewInit {
+  // lifecycle hooks
+
+  ngAfterViewInit() {
+    console.log(this.rowService.vampireDowntimeRows);
     for (let index = 0; index < 7; index++) {
       this.addRow();
-      
     }
+    console.log(this.rowService.vampireDowntimeRows);
+    this.dataSource = new MatTableDataSource(
+      this.rowService.vampireDowntimeRows
+    );
+    console.log(this.dataSource.data);
+    this.dataSource.paginator = this.paginator;
   }
+
+  // properties
+
   displayedColumns: string[] = [
     'position',
     'Turf',
@@ -24,9 +36,14 @@ export class RandomTableComponent implements OnInit{
     'NPC Faction',
     'Required Roll',
   ];
-  // dataSource!:VampireDowntimeElement[];
-  dataSource = new MatTableDataSource<VampireDowntimeElement>([]);
+
+  dataSource = new MatTableDataSource<VampireDowntimeElement>();
   public rowService = inject(RowService);
+
+  @ViewChild('paginator')
+  paginator!: MatPaginator;
+
+  // methods
 
   addRow() {
     this.rowService
@@ -41,15 +58,15 @@ export class RandomTableComponent implements OnInit{
     this.displayedColumns.forEach((element) => {
       if (element !== 'position' && element !== 'Required Roll') {
         text += `${element};`;
-      }else if(element === 'Required Roll'){
+      } else if (element === 'Required Roll') {
         text += `${element}`;
       }
     });
     text += '\n';
 
-    this.rowService.vampireDowntimeRows.forEach(element=>{
+    this.rowService.vampireDowntimeRows.forEach((element) => {
       text += `${element.turf};${element.eventType};${element.reward};${element.rewardNumberRoll};${element.npcFaction};${element.requiredRoll}\n`;
-    })
+    });
     return text;
   }
 }
